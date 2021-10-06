aaf06d83a7784ec356c66fda1457ce32deb09643
Bernheim1 <agustinbernheim8@gmail.com>
2021-10-06 19:10:39 -0300

    primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDF32e9BH9QFzCWmAu_9EXa18n4Y90mkIs",
+    authDomain: "bernheim-pp-lab4.firebaseapp.com",
+    projectId: "bernheim-pp-lab4",
+    storageBucket: "bernheim-pp-lab4.appspot.com",
+    messagingSenderId: "894098973526",
+    appId: "1:894098973526:web:35526e5a552a09044aacf7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDF32e9BH9QFzCWmAu_9EXa18n4Y90mkIs",
+    authDomain: "bernheim-pp-lab4.firebaseapp.com",
+    projectId: "bernheim-pp-lab4",
+    storageBucket: "bernheim-pp-lab4.appspot.com",
+    messagingSenderId: "894098973526",
+    appId: "1:894098973526:web:35526e5a552a09044aacf7"
+  }
 };
 
 /*
